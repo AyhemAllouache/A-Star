92006532e3f4095f458856e26676d645b4ccebfb
Ayhem <ja_allouache@esi.dz>
2023-12-08 18:22:18 +0100

    4 and 8 Connectivity

---
+++ aStar-4.py
@@ -0,0 +1,246 @@
+import pygame
+import heapq
+import time
+import math
+
+# Initialize Pygame
+pygame.init()
+pygame.font.init()  # Initialize the font module
+
+# Window settings
+WIDTH, HEIGHT = 800, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("A* Path Finding Algorithm")
+
+# Colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+# Grid settings
+ROWS = 30
+GAP = WIDTH // ROWS
+FONT = pygame.font.SysFont('Arial', 20)  # Create a font object
+
+class Node:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.x = row * GAP
+        self.y = col * GAP
+        self.color = WHITE
+        self.neighbors = []
+
+    def get_pos(self):
+        return self.row, self.col
+
+    def is_closed(self):
+        return self.color == RED
+
+    def is_open(self):
+        return self.color == GREEN
+
+    def is_barrier(self):
+        return self.color == BLACK
+
+    def is_start(self):
+        return self.color == ORANGE
+
+    def is_end(self):
+        return self.color == TURQUOISE
+
+    def reset(self):
+        self.color = WHITE
+
+    def make_start(self):
+        self.color = ORANGE
+
+    def make_closed(self):
+        self.color = RED
+
+    def make_open(self):
+        self.color = GREEN
+
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_end(self):
+        self.color = TURQUOISE
+
+    def make_path(self):
+        self.color = PURPLE
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, GAP, GAP))
+
+    def update_neighbors(self, grid):
+        self.neighbors = []
+        if self.row < ROWS - 1 and not grid[self.row + 1][self.col].is_barrier():  # Down
+            self.neighbors.append(grid[self.row + 1][self.col])
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():  # Up
+            self.neighbors.append(grid[self.row - 1][self.col])
+        if self.col < ROWS - 1 and not grid[self.row][self.col + 1].is_barrier():  # Right
+            self.neighbors.append(grid[self.row][self.col + 1])
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():  # Left
+            self.neighbors.append(grid[self.row][self.col - 1])
+
+def heuristic(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def reconstruct_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
+
+def algorithm(draw, grid, start, end):
+    start_time = time.time()  # Start time
+    count = 0
+    open_set = []
+    heapq.heappush(open_set, (0, count, start))
+    came_from = {}
+    g_score = {node: float("inf") for row in grid for node in row}
+    g_score[start] = 0
+    f_score = {node: float("inf") for row in grid for node in row}
+    f_score[start] = heuristic(start.get_pos(), end.get_pos())
+
+    open_set_hash = {start}
+
+    while not len(open_set) == 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = heapq.heappop(open_set)[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            end_time = time.time()  # End time
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
+            return True, end_time - start_time  # Return the time taken
+
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + heuristic(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    heapq.heappush(open_set, (f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+
+        draw()
+
+        if current != start:
+            current.make_closed()
+
+    return False, 0
+
+def make_grid(rows, width):
+    grid = []
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node(i, j)
+            grid[i].append(node)
+    return grid
+
+def draw_grid(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+        for j in range(rows):
+            pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+
+def draw(win, grid, rows, width, time_taken=None):
+    win.fill(WHITE)
+
+    for row in grid:
+        for node in row:
+            node.draw(win)
+
+    draw_grid(win, rows, width)
+
+    if time_taken is not None:
+        time_text = FONT.render(f"Time taken: {time_taken:.2f} seconds", True, BLACK)
+        win.blit(time_text, (10, 10))
+
+    pygame.display.update()
+
+def get_clicked_pos(pos, rows, width):
+    gap = width // rows
+    y, x = pos
+    row = y // gap
+    col = x // gap
+    return row, col
+
+def main(win, width):
+    ROWS = 30
+    grid = make_grid(ROWS, width)
+
+    start = None
+    end = None
+    time_taken = None
+
+    run = True
+    while run:
+        draw(win, grid, ROWS, width, time_taken)
+        for event in pygame.event.get():
+            if event.type is pygame.QUIT:
+                run = False
+
+            if pygame.mouse.get_pressed()[0]:  # LEFT
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                node = grid[row][col]
+                if not start and node != end:
+                    start = node
+                    start.make_start()
+                elif not end and node != start:
+                    end = node
+                    end.make_end()
+                elif node != end and node != start:
+                    node.make_barrier()
+
+            elif pygame.mouse.get_pressed()[2]:  # RIGHT
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                node = grid[row][col]
+                node.reset()
+                if node == start:
+                    start = None
+                elif node == end:
+                    end = None
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and start and end:
+                    for row in grid:
+                        for node in row:
+                            node.update_neighbors(grid)
+
+                    path_found, time_taken = algorithm(lambda: draw(win, grid, ROWS, width, time_taken), grid, start, end)
+                    if not path_found:
+                        time_taken = None
+
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
+                    time_taken = None
+
+    pygame.quit()
+
+main(WIN, WIDTH)
